b80f908a7b3abf1307d789586aed2e7fb7a100d5
bruno <brunocordeiro180@gmail.com>
2019-01-06 00:45:39 -0200

    credito adicionado quando reserva eh cancelada

--- wp-content/themes/fitnesszone-child/page-minhas-reservas.php
+++ wp-content/themes/fitnesszone-child/page-minhas-reservas.php
@@ -8,7 +8,7 @@
 
 <?php
 
-if(is_array(get_userdata(wp_get_current_user()->ID)->roles)){
+  if(is_array(get_userdata(wp_get_current_user()->ID)->roles)){
     $capitalized_value = strtoupper(get_userdata(wp_get_current_user()->ID)->roles[0]);
   } else {
     $capitalized_value = strtoupper(get_userdata(wp_get_current_user()->ID)->roles);
@@ -16,16 +16,28 @@ if(is_array(get_userdata(wp_get_current_user()->ID)->roles)){
   
   if(is_user_logged_in() && strpos(strtoupper($capitalized_value), 'bloqueado') !== true ): ?>
 
-
+    <?php 
+    
+        $user = wp_get_current_user()->ID; 
+        $credito = get_field( 'credito', 'user_' . $user );
+    
+    ?>
 
 <section class="reservation__block">
     <div class="container">
         <div class="row">
-            <h2 style="float: left;">Minhas Reservas</h2>
-            <form action="../agendamento">
-                <input type="submit" value="Nova Reserva" />
-            </form>
+            <div class="col-sm">
+                <h2>Minhas Reservas</h2>
+                <p>Crédito: $<?php echo number_format($credito, 2); ?></p>
+            </div>
+            <div style="width: 0%;" class="col-sm">
+            <!-- <h4 style="float: right;">Crédito: $0,00</h4> -->
+                <form action="../agendamento">
+                    <input type="submit" value="Nova Reserva" />
+                </form>
+            </div>
         </div>
+        <div class="row">
         <?php
             $user = get_currentuserinfo()->ID;
             $args = array(
@@ -36,19 +48,22 @@ if(is_array(get_userdata(wp_get_current_user()->ID)->roles)){
            $reservas = new WP_Query( $args );
             if($reservas->have_posts() ) :
             ?>
-            <table style="width: 60%; margin: auto;" class="table-reservas">
+            <table style="width: 60%;" class="table-reservas">
                 <?php
+                $reserva_id = 0;
                 while($reservas->have_posts() ) :
                    $reservas->the_post();
                    $id = get_the_id();
+                   if(get_field("status", $id)){
                     ?>
                     <tr>
                         <td style="padding-bottom: 20px; padding-top: 0px;"><h4><?php echo the_title();  ?></h4></td>
                         <td style="padding-bottom: 20px; padding-top: 0px;"><h4><?php echo get_field("data", $id);  ?></h4></td>
                         <td style="padding-bottom: 20px; padding-top: 0px;"><a href="../editar-reserva/?reservaid=<?php echo $id; ?>">Editar Reserva</a></td>
-                        <td ><button id="<?php echo $id; ?>" onclick="cancelarReserva(<?php echo get_the_id();?>);" href="">Cancelar Reserva</button></td>
+                        <td ><button id="<?php echo $id; ?>" onclick="cancelarReserva(<?php echo get_the_id(); $reserva_id = get_the_id();?>);" href="">Cancelar Reserva</button></td>
                     </tr>
                     <?php
+                   }
                 endwhile;
                 wp_reset_postdata();
                 ?>
@@ -64,10 +79,10 @@ if(is_array(get_userdata(wp_get_current_user()->ID)->roles)){
 <script>
 
     function cancelarReserva(reserva_id) {
-
+       
         jQuery.ajax({
             type: "POST",
-            url: "https://brapadel.com.br/minhas-reservas",
+            url: window.location.href.split('?')[0],
             data: { reserva_id: reserva_id }
         }).done(function( msg ) {
             location.reload();
@@ -109,6 +124,10 @@ if(is_array(get_userdata(wp_get_current_user()->ID)->roles)){
     $field_key = "status";
     $value = 0;
     update_field( $field_key, $value, $_POST['reserva_id']);
+    
+    $valor_reserva = 0;
+    $valor_reserva = get_field('valor', $_POST['reserva_id']); 
+    update_field('credito', floatval($credito) + floatval($valor_reserva), 'user_' . $user); 
 
 ?>
 
