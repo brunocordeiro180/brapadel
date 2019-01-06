a2a3a28ea58b2710a46e561fd14f66d6f80d1357
bruno <brunocordeiro180@gmail.com>
2019-01-06 00:57:56 -0200

    bootstrap removido

--- wp-content/themes/fitnesszone-child/functions.php
+++ wp-content/themes/fitnesszone-child/functions.php
@@ -4,7 +4,7 @@
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' ); 
 
 function enqueue_parent_styles() {
-  wp_enqueue_style('bootstrap', get_stylesheet_directory_uri().'/css/reservas/bootstrap.min.css'); 
+  // wp_enqueue_style('bootstrap', get_stylesheet_directory_uri().'/css/reservas/bootstrap.min.css'); 
   wp_enqueue_style('parent-style', get_stylesheet_directory_uri().'/css/reservas/jsCalendar.css'); 
   wp_enqueue_style( 'parent-style2', get_template_directory_uri().'/style.css' ); 
   wp_enqueue_style('parent-style3', get_stylesheet_directory_uri().'/css/reservas/pickaday.css');
